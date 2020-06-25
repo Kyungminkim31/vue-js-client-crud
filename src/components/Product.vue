6b20b0cf98c737a4db3de2d2c1161ade0a8b2c6c
Kyungmin Kim <kyungmin.kim.31@gmail.com>
2020-06-25 14:10:45 +0900

    상품상세조회 양식 변경

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="currentProduct" class="edit-form">
-    <h4>상품</h4>
+    <h4>상품 상세 정보</h4>
     <form>
       <div class="form-group row">
         <label for="name" class="col-sm-2 col-form-label"> 상품명 </label>
@@ -9,7 +9,6 @@
           v-model="currentProduct.name" />
         </div>
       </div>
-
       <div class="form-group row">
         <label for="description" class="col-sm-2 col-form-label"> 내용 </label>
         <div class="col-sm-10">
@@ -39,22 +38,25 @@
         </div>
       </fieldset>
     </form>
-    <div class="float-right">
-      <button type="submit" class="btn btn-primary mr-2"
+    <div class="text-right">
+      <button type="submit" class="btn btn-link mr-2"
         @click="updateProduct" >
         수정
       </button>
-      <button class="btn btn-primary mr-2"
+      <button class="btn btn-link mr-2"
         @click="deleteProduct">
         삭제
       </button>
     </div>
-    <p> {{ message }} </p>
+    <div class="align-center text-center mt-3">
+      <transition name="fade">
+        <div :class="[isSuccess ?  successClass : errorClass ]" v-if="show"> {{ message }} </div>
+      </transition>
+    </div>
   </div>
-
   <div v-else>
     <br/>
-    <p> 강좌 리스트를 클릭해 주세요.</p>
+    <p> 상품 데이터가 존재 하지 않습니다. </p>
   </div>
 </template>
 
@@ -66,6 +68,11 @@ export default {
   data(){
     return {
       currentProduct: null,
+      message: '',
+      show: true,
+      successClass: 'alert alert-success',
+      errorClass: 'alert alert-danger',
+      isSuccess: true,
     };
   },  
   methods: {
@@ -83,10 +90,17 @@ export default {
       ProductDataService.update( this.currentProduct.id, this.currentProduct)
       .then(response => {
         console.log(response.data);
-        this.message = '상품 정보가 수정이 되었습니다.';
+        this.isSuccess = true;
+        this.message = response.data.message;
+        this.show = true
+        setTimeout(()=>{this.show = false}, 2000);
       })
       .catch( e => {
         console.log(e);
+        this.isSuccess = false;
+        this.message = e;
+        this.show = true
+        setTimeout(()=>{this.show = false}, 2000);
       });
     },
     deleteProduct(){
@@ -98,10 +112,13 @@ export default {
       .catch( e => {
         console.log(e);
       });
-    }
+    },
+    showMessage(inputMsg){
+      this.message = inputMsg;
+    },
   },
   mounted() {
-    this.message ='';
+    this.message = '';
     this.getProduct(this.$route.params.id);
   }
 };
@@ -111,5 +128,12 @@ export default {
 .edit-form{
   .max-width: 300px;
   margin: auto;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
