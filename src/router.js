a394d53663719c2d5dd0dc7f101d72fea7b95a3d
Kyungmin Kim <kyungmin.kim.31@gmail.com>
2020-07-08 15:35:11 +0900

    fix error to set token data into store storage

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import Router from "vue-router";
 
 Vue.use(Router);
 
-export const router =  new Router({
+const router =  new Router({
   mode: "history",
   routes: [
     {
@@ -66,16 +66,14 @@ export const router =  new Router({
 
 router.beforeEach((to, from, next) => {
   const publicPages = ['/login', '/register', '/home'];
-
   const authRequired = !publicPages.includes(to.path);
-
   const loggedIn = localStorage.getItem('user');
 
-  if(authRequired && !loggedIn){
+  if (authRequired && !loggedIn) {
     next('/login');
   } else {
     next();
   }
 });
 
-export default router
+export default router;
